b79b50af069493ca9d1ffefbbade730c42480d28
Vince <vincevandoorn@gmail.com>
2021-02-05 15:09:22 +0100

    script

--- php/cursussenbekijkenleerling/leerlingcursusscript.php
+++ php/cursussenbekijkenleerling/leerlingcursusscript.php
@@ -2,6 +2,7 @@
 include("../connect_db.php");
 include("../functions.php");
 
+$klas = "AM2A";
 $sql = "SELECT * FROM klasregel WHERE Klas = '$klas' ";
 
 $result = $conn->query($sql);
@@ -10,8 +11,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      echo "Cursusnaam: " . $row["CursusNaam"]. "<br>";
     }
-
-
+    }
 ?>
